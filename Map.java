9b716ec1a34b906e3b081280de563ed9e444f48a
KoheiSekiya <44878180+KoheiSekiya@users.noreply.github.com>
2018-11-09 15:38:06 +0900

    Add files via upload

---
+++ Map.java
@@ -0,0 +1,8 @@
+/**
+ * Created by takumi on 2018/11/02.
+ */
+public class Map {
+	int map[][] = new int [5][5];
+
+}
+
---
+++ koukadaiRaider.java
@@ -0,0 +1,26 @@
+import com.sun.media.jfxmedia.events.PlayerStateEvent;
+
+/**
+ * Created by takumi on 2018/11/02.
+ */
+// TEST
+public class koukadaiRaider {
+    public static void main(String[] args) {
+        System.out.println("ゲーム開始");
+        Map map = new Map();    //ダンジョンの初期化
+        Move mv = new Move();
+        Battle bt = new Battle();
+    }
+}
+
+public class player {
+    int PlayerHP, PlayerStealth, Weapon, Smoke, Treasure;
+    
+}
+
+
+
+
+
+
+
