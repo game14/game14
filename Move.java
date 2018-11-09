b3a13eaec39bb7683f779e24e2f4ab68f82eeb0a
KoheiSekiya <44878180+KoheiSekiya@users.noreply.github.com>
2018-11-09 17:38:11 +0900

    Update Move.java

--- Move.java
+++ Move.java
@@ -3,27 +3,27 @@
  */
 import java.util.*;
 
-public class Move {
-	System.out.print("移動する方向を選んでください");
+public class Move extends player {
+
 	Scanner sc = new Scanner(System.in);
 	String str = sc.nextLine();
 
-	/*if(str==){//レーダーの処理
-		System.out.print(レーダーを使った);
-	}*/
-	
 	switch(str){
+		case s:
+			System.out.print(レーダーを使った！);
+			return ++x, --x, ++y, --y;
+			break;
 		case u: 
-			return  
+			return x, --y;
 			break;
 		case r:
-
+			return ++x, y;
 			break;
 		case l:
-
+			return --x, y;
 			break;
 		case d:
-			
+			return x, ++y;
 			break;
 	}
 }
