5898ce01d515864e57cd2b85bbd8d516cd87ff70
jeongfish <44885681+jeongfish@users.noreply.github.com>
2018-11-09 15:45:46 +0900

    Update koukadaiRaider.java

--- koukadaiRaider.java
+++ koukadaiRaider.java
@@ -1,21 +1,73 @@
 import com.sun.media.jfxmedia.events.PlayerStateEvent;
 
-/**
- * Created by takumi on 2018/11/02.
- */
-// TEST
-public class koukadaiRaider {
-    public static void main(String[] args) {
-        System.out.println("ゲーム開始");
-        Map map = new Map();    //ダンジョンの初期化
-        Move mv = new Move();　//プレイヤーの移動
-        Battle bt = new Battle(); //戦闘の呼び出し
-    }
-}
+import java.util.Scanner;
 
-public class player {
-    int PlayerHP, PlayerStealth, Weapon, Smoke, Treasure;
+public class lol {
+	public static final Scanner USERINPUT = new Scanner(System.in);
+	
+    public static void main(String[] args) {
+        mainMenu();
+        //Map map = new Map();
+        //Move mv = new Move();
+        //Battle bt = new Battle();
+    }
+    public static void mainMenu() {
+    	welcome();
+        boolean status = false;
+        do {
+            String selection = USERINPUT.nextLine();
+            switch (selection) {
+                case "1":
+                    newGame();
+                    status = true;
+                    break;
+                case "2":
+                    rule();
+                    status = true;
+                    break;    
+                case "3":
+                    System.exit(0);
+            }
+
+        } while (status == false);
+    }
     
+    public static void welcome() {
+        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+                + "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+        System.out.println("-----------------------");
+        System.out.println("       MAIN MENU       ");
+        System.out.println("-----------------------");
+        System.out.println("Welcome!");
+        System.out.println();
+        System.out.println(" 1.     New Game        ");
+        System.out.println(" 2.     Rule        ");
+        System.out.println(" 3.     Exit        ");
+        System.out.println();
+        System.out.print(" Make a Selection: ");
+        System.out.println();
+        System.out.println();
+    }
+    
+    public static void newGame() {
+    	//newGame
+    	//boolean status = false;
+    	System.out.println("GAME");
+    }
+    public static void rule() {
+    	//rule
+    	//boolean status = false;
+    	System.out.println("RULES: ");
+    	System.out.println("1) This is a game ");
+    	System.out.println("2) bla bla bla");
+    	System.out.println("3) bla bla");
+    	System.out.println("Input any key for Main Menu");
+        USERINPUT.nextLine();
+        mainMenu();
+    }
+    public class player {
+        int PlayerHP, PlayerStealth, Weapon, Smoke, Treasure;
+    }
 }
 
 
