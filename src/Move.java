0a87629a41e383666182fa7990873801b5b66ea2
KoheiSekiya <44878180+KoheiSekiya@users.noreply.github.com>
2018-11-09 17:50:40 +0900

    Update Move.java

--- src/Move.java
+++ src/Move.java
@@ -1,5 +1,29 @@
 /**
  * Created by takumi on 2018/11/02.
  */
-public class Move {
+import java.util.*;
+
+public class Move extends player {
+
+	Scanner sc = new Scanner(System.in);
+	String str = sc.nextLine();
+
+	switch(str){
+		case s:
+			System.out.print(レーダーを使った！);
+			return ++x, --x, ++y, --y;
+			break;
+		case u: 
+			return x, --y;
+			break;
+		case r:
+			return ++x, y;
+			break;
+		case l:
+			return --x, y;
+			break;
+		case d:
+			return x, ++y;
+			break;
+	}
 }
