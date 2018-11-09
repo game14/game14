00eca9f6a981b4dd23cb61dd5087c9e4b5764009
KoheiSekiya <44878180+KoheiSekiya@users.noreply.github.com>
2018-11-09 16:46:51 +0900

    Update Move.java

--- Move.java
+++ Move.java
@@ -1,5 +1,30 @@
 /**
  * Created by takumi on 2018/11/02.
  */
+import java.util.*;
+
 public class Move {
+	Scanner sc = new Scanner(System.in);
+	String str = sc.nextLine();
+
+	/*if(str==){//レーダーの処理
+		System.out.print(レーダーを使った);
+	}*/
+
+	System.out.print("移動する方向を選んでください");
+	str = sc.next();
+	switch(str){
+		case u: 
+			return  
+			break;
+		case r:
+
+			break;
+		case l:
+
+			break;
+		case d:
+			
+			break;
+	}
 }
