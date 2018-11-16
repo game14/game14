817d728c8dfc60515b34c1384b49e7c572b8edc4
KoheiSekiya <44878180+KoheiSekiya@users.noreply.github.com>
2018-11-16 13:54:25 +0900

    Update Map.java

--- Map.java
+++ Map.java
@@ -1,8 +1,20 @@
-/**
- * Created by takumi on 2018/11/02.
- */
-public class Map {
-	int map[][] = new int [5][5];
+package src;
 
+
+public class map {
+	public int enemyhp;
+	public int bosshp;
+	public static int x = 2;
+    public static int y = 0;
+	
+	public map() {
+		bosshp = koukadaiRaider.bossHP;
+		enemyhp = koukadaiRaider.enemyHP;
+		
+		String[][] mapz = new String[5][5];
+		mapz[2][0] = "START";
+		mapz[4][0] = "BOSS";
+		mapz[1][4] = "SWORD";
+ 	}
+    
 }
-
