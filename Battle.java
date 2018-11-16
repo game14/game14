2a1aff7cbffa2ce1773898291a851e5b41e0a48d
KoheiSekiya <44878180+KoheiSekiya@users.noreply.github.com>
2018-11-16 13:53:55 +0900

    Update Battle.java

--- Battle.java
+++ Battle.java
@@ -1,5 +1,11 @@
-/**
- * Created by takumi on 2018/11/02.
- */
-public class Battle {
+package src;
+
+public class battle {
+	public int hp;
+	void checkHP() {
+		this.hp = 15;
+	}
+	public int fight() {
+		return this.hp = 0;
+	}
 }
